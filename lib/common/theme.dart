d8bc94aabbd0811190cb47eb9c951ddb8c74b16c
ruyi <tan_ruyi@hotmail.com>
2023-02-02 15:46:24 +0800

    theme done

--- lib/common/theme.dart
+++ lib/common/theme.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
 
-final appTheme = ThemeData(
-  colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
-  fontFamily: 'Bitter'
+ThemeData appTheme = ThemeData(
+  colorScheme: const ColorScheme(
+      brightness: Brightness.light,
+      primary: Color.fromARGB(255, 234, 158, 91),
+      onPrimary: Color.fromARGB(255, 255, 255, 255),
+      secondary: Color.fromARGB(255, 255, 245, 236),
+      onSecondary: Color.fromARGB(255, 234, 158, 91),
+      error: Color.fromARGB(255, 248, 224, 203),
+      onError: Colors.red,
+      background: Color.fromARGB(255, 255, 252, 250),
+      onBackground: Color.fromARGB(255, 76, 61, 48),
+      surface: Color.fromARGB(255, 255, 255, 255),
+      onSurface: Color.fromARGB(255, 76, 61, 48),
+      outline: Color.fromARGB(255, 76, 61, 48),
+      outlineVariant: Color.fromARGB(255, 76, 61, 48)),
+  fontFamily: 'Bitter',
+  textTheme: const TextTheme(
+    bodyLarge: TextStyle(
+      color: Color.fromARGB(255, 76, 61, 48),
+    ),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+      backgroundColor: const Color.fromARGB(255, 234, 158, 91),
+    ),
+  ),
 );
